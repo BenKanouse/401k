b32358461e0e71557ddcc9d796b121ab009058c5
Ben Kanouse <kanobt61@gmail.com>
2023-06-30 12:09:09 -0400

    Improve the output

--- 401k.rb
+++ 401k.rb
@@ -3,8 +3,13 @@
 class ReturnOverTime
   def self.run(years, current_year, start, rate, contribution)
     new = start * (1 + rate) + contribution
-    puts "years left: #{years-current_year}, $#{start}"
+    string_money = start.to_i.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+    puts "year ##{years - current_year}: $#{string_money}.00"
     self.run(years, current_year - 1, new, rate, contribution) if current_year > 0
   end
 end
-puts ReturnOverTime.run(30, 30, 100_000, 0.08, 10_000)
+years = 35
+starting_amount = 100_000
+intrest_rate = 0.08
+yearly_contribution = 25_000
+puts ReturnOverTime.run(years, years, starting_amount, intrest_rate, yearly_contribution)
